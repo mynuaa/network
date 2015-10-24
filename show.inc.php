775a8e0da0df7ee9697d19f317876404ac0c5c4d
Rex Zeng <rex.fgtsky@qq.com>
2015-10-24 11:41:29 +0800

    添加浏览量

--- show.inc.php
+++ show.inc.php
@@ -4,5 +4,7 @@ if (!defined('IN_DISCUZ')) exit('Access Denied');
 if (!isset($_GET['nid'])) exit('Undefined ID');
 
 $result = DB::fetch_first("SELECT * FROM `cdb_network` WHERE `nid` = {$_GET['nid']}");
+DB::query("UPDATE `cdb_network` SET `pageview` = " . ($result['pageview'] + 1) . " WHERE `nid` = {$_GET['nid']}");
+$result['pageview'] += 1;
 
 include template('network:show');
