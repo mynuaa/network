ac66ee34195bf3153423348ecd1d2dfb95e8c38b
Rex Zeng <rex.fgtsky@qq.com>
2015-10-26 15:35:26 +0800

    添加文字

--- network.inc.php
+++ network.inc.php
@@ -5,7 +5,8 @@ if(!defined('IN_DISCUZ')) exit('Access Denied');
 $cartoons = DB::fetch_all("SELECT * FROM `cdb_network` WHERE `groupname` = '动漫'");
 $photos = DB::fetch_all("SELECT * FROM `cdb_network` WHERE `groupname` = '摄影'");
 $movies = DB::fetch_all("SELECT * FROM `cdb_network` WHERE `groupname` = '微电影'");
-$businesses = DB::fetch_all("SELECT * FROM `cdb_network` WHERE `groupname` = '创新创业'");
+// $businesses = DB::fetch_all("SELECT * FROM `cdb_network` WHERE `groupname` = '创新创业'");
+$business = [];
 $articles = DB::fetch_all("SELECT * FROM `cdb_network` WHERE `groupname` = '网文'");
 
 include template('network:network');
--- show.inc.php
+++ show.inc.php
@@ -7,4 +7,21 @@ $result = DB::fetch_first("SELECT * FROM `cdb_network` WHERE `nid` = {$_GET['nid
 DB::query("UPDATE `cdb_network` SET `pageview` = " . ($result['pageview'] + 1) . " WHERE `nid` = {$_GET['nid']}");
 $result['pageview'] += 1;
 
+if (preg_match('/^photos\|(\d+)$/', $result['content'], $matches)) {
+	$count = intval($matches[1]);
+	$ts = '';
+	for ($i = 1; $i <= $count; $i++)
+		$ts .= "<p><img src=\"source/plugin/network/resources/{$result['groupname']}/{$result['title']}/{$i}.jpg\"></p>";
+	$result['content'] = $ts;
+}
+else if (preg_match('/^video$/', $result['content'], $matches)) {
+	// 
+}
+else {
+	$paras = explode("\n", $result['content']);
+	$result['content'] = '<div class="article">' . implode(array_map(function ($str) {
+		return "<p>{$str}</p>";
+	}, $paras)) . '</div>';
+}
+
 include template('network:show');
