8a7dae143a861b020ab2f138900c0a78e496942a
Rex Zeng <rex.fgtsky@qq.com>
2015-10-26 15:37:39 +0800

    修改动漫为漫画

--- network.inc.php
+++ network.inc.php
@@ -2,7 +2,7 @@
 
 if(!defined('IN_DISCUZ')) exit('Access Denied');
 
-$cartoons = DB::fetch_all("SELECT * FROM `cdb_network` WHERE `groupname` = '动漫'");
+$cartoons = DB::fetch_all("SELECT * FROM `cdb_network` WHERE `groupname` = '漫画'");
 $photos = DB::fetch_all("SELECT * FROM `cdb_network` WHERE `groupname` = '摄影'");
 $movies = DB::fetch_all("SELECT * FROM `cdb_network` WHERE `groupname` = '微电影'");
 // $businesses = DB::fetch_all("SELECT * FROM `cdb_network` WHERE `groupname` = '创新创业'");
